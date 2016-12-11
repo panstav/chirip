fe345666eef7f7931b90735b9dde25d4e701ebe7
Stav Geffen <stavgeffen@gmail.com>
2016-12-11 23:00:42 +0200

    feat(init-user): adds profile pic, name & handle to each note

--- client/initial-state.js
+++ client/initial-state.js
@@ -1,6 +1,7 @@
 const local = require('store');
 
 module.exports = {
+	user: local.get('user'),
 	notes: local.get('notes') || [],
 	newNote: { content: '', tags: [], createdAt: 0 }
 };
--- client/lib/populate-notes.js
+++ client/lib/populate-notes.js
@@ -13,12 +13,24 @@ function populateNotes(notes){
 function noteElem(note){
 	return `
 <article data-note-id="${ note.id }" class="cf bg-white mt3 br2 overflow-hidden">
-	<div class="f6 light-silver pa2">${ humanDate(note.createdAt) }</div>
-	<p class="f4 mh2 pa2">${ note.content }</p>
-	<div class="b--light-blue bt bw1"><div data-role="actions-container" class="light-silver overflow-hidden">
-			<a data-action="edit-note" class="dib ph3 pv2"><img class="db h1 w1" src="/svg/edit.svg"></a>
-			<a data-action="delete-note" class="dib ph3 pv2"><img class="db h1 w1" src="/svg/delete.svg"></a>
-	</div></div>
+  <div class="fl">
+  	<a class="dib ma2 overflow-hidden w3 br2">
+	    <img src="https://placekitten.com/g/200/200" class="db h3 w3">
+	  </a>
+	</div>
+	<div data-role="note-content" class="fl mt2">
+		
+		<span class="fw6 mr2">${note.author.name}</span>
+		<span data-role="author-handle" class="light-silver">${note.author.handle}</span>
+		<div class="fr f6 light-silver mr2">${ humanDate(note.createdAt) }</div>
+		
+		<p class="f4 mh2 pa2">${ note.content }</p>
+		<div class="b--light-blue bt bw1"><div data-role="actions-container" class="light-silver overflow-hidden">
+				<a data-action="edit-note" class="dib ph3 pv2"><img class="db h1 w1" src="/svg/edit.svg"></a>
+				<a data-action="delete-note" class="dib ph3 pv2"><img class="db h1 w1" src="/svg/delete.svg"></a>
+		</div></div>
+		
+	</div>
 </article>
 `;
 }
