03ccbe14a9982222e11cc7620a5e7a09a6a1aa6c
Stav Geffen <stavgeffen@gmail.com>
2017-01-14 22:46:47 +0200

    fix(note-date): imports twitter-timeago to change fallback date format

--- client/lib/render-note.js
+++ client/lib/render-note.js
@@ -1,5 +1,3 @@
-const timeAgo = require('twitter-timeago');
-
 module.exports = renderNote;
 
 function renderNote(note){
@@ -26,5 +24,43 @@ function renderNote(note){
 	<a data-action="delete-note" title="Delete note" class="dib pl2 pv2 pointer"><img class="db h1 w1" src="/svg/delete.svg"></a>
 </div>
 `;
+
+}
+
+function timeAgo(timestamp){
+
+	// Times in milliseconds
+	const second = 1e3
+		, minute = 6e4
+		, hour = 36e5
+		, day = 864e5
+		, week = 6048e5;
+
+	if (timestamp instanceof Date) {
+		timestamp = timestamp.getTime();
+	}
+
+	if (typeof timestamp === 'string') {
+		timestamp = new Date(timestamp).getTime();
+	}
+
+	const diff = Math.abs(timestamp - Date.now());
+
+	if (diff <= second) {
+		return '1s';
+	} else if (diff < minute) {
+		return Math.floor(diff / 1000) + 's';
+	} else if (diff < hour) {
+		return Math.floor(diff / 1000 / 60) + 'm';
+	} else if (diff < day) {
+		return Math.floor(diff / 1000 / 3600) + 'h';
+	} else {
+		if (diff < week) {
+			return Math.floor(diff / 1000 / 86400) + 'd';
+		} else {
+			const d = new Date(timestamp);
+			return (d.getDate()) + '/' + (d.getMonth() + 1) + '/' + d.getFullYear().toString().substr(2);
+		}
+	}
 
 }
